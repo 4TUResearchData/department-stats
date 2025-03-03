4b63107b329e4b2dcf62e5c6709f65419accf53f
Aleksandra Wilczynska <a.e.wilczynska@tudelft.nl>
2025-03-03 15:52:59 +0100

    add delft departments

--- src/data-by-departments.R
+++ src/data-by-departments.R
@@ -1,3 +1,4 @@
+# Libraries used
 library(httr2)
 library(jsonlite)
 library(dplyr)
@@ -5,22 +6,41 @@ library(purrr)
 library(tidyr)
 library(stringr)
 library(ggplot2)
+library(readr)
+
+# Make the API request to find articles from a certain group
+
+get_all_datasets_group <- function(api_url, 
+                                   limit = 1000,
+                                   group, 
+                                   published_since) {
+
+  datasets <- request(api_url) |>
+    req_url_query("limit"=limit,
+                  "group"= group,
+                  "published_since"= published_since) |>
+                    req_perform() |>
+                    resp_body_string() |>
+                    fromJSON()
+  return(datasets)
+
+}
 
 api_url <- "https://data.4tu.nl/v2/articles"
+limit <- 1000
+group <- c('eindhoven' = 28589, # eindhoven
+            'delft' = 28586, 
+            'twente' = 28592,
+            'wur' = 28595
+          )
 
-# Make the API request
-all_datasets <- request(api_url) |>
-  req_url_query("limit"=1000,
-                "group"= 28589, # eindhoven
-                "published_since"= "2024-01-01") |>
-  req_perform() |>
-  resp_body_string() |>
-  fromJSON()
+start_date <- "2024-01-01"
 
-# make a loop for each dataset from eindhoven
+all_datasets <- get_all_datasets_group(api_url, limit, group['delft'], start_date)
+
+# make a loop for each dataset to retrieve the details from the API
 
 datasets<- list()
-
 
 for (dataset in all_datasets$uuid) {
 
@@ -30,15 +50,24 @@ for (dataset in all_datasets$uuid) {
     resp_body_string() |>
     fromJSON()
   
-  organizations <-response$custom_fields|>
-    filter(name == "Organizations")|>
-    select(value) 
+  if( !is.null(response$custom_fields)){
+    organizations <-response$custom_fields|>
+      filter(name == "Organizations")|>
+      pull(value) 
+  }
+  else {
+    organizations <- ""
+  }
 
-  datasets[[dataset]]<- tibble(uuid = dataset, 
-                               doi = response$doi, 
-                               organization = organizations$value)
+  datasets[[dataset]]<- tibble(uuid = dataset
+                               ,doi = response$doi 
+                               ,title = response$title
+                               ,created_date = response$created_date
+                               ,modified_date = response$modified_date
+                               ,organization = organizations)
 }
 
+#  Convert to a data.frame
 org <- sapply(datasets, function(x) unlist(x$organization) ) 
 
 datasets <- datasets |> 
@@ -46,36 +75,55 @@ datasets <- datasets |>
   bind_rows() |>
   mutate(organization = org)
 
-pattern <- "\\b(Eindhoven University of Technology,?|TU Eindhoven,?)\\b"
+# Organisation information text cleaning
+uni_pattern <- c(eindhoven = "(Eindhoven University of Technology|TU Eindhoven)",
+                 delft = "(Delft University of Technology|TU Delft)")
 
 datasets<- datasets|>
-  mutate(organization_clean = organization |> str_remove_all( pattern)|>
-                              str_replace_all("Faculty", "Department")|> 
-                              str_replace_all("&", "and")|> 
-                              str_remove_all("[[:punct:]]")|>
-                              tolower())
+  mutate(organization_clean = organization |> 
+                                str_remove_all(unname(uni_pattern['delft']))|>
+                                str_replace_all("&", "and")|> 
+                                str_remove_all("[[:punct:]]")|>
+                                tolower())
 
 datasets|>
   count(organization_clean) |>
   print(n = 50)
 
-departments <- c( "Applied Physics",
-"Biomedical Engineering", 
-"Built Environment", 
-"Chemical Engineering",
-"Electrical Engineering", 
-"Industrial Design", 
-"Industrial Engineering", 
-"Mathematics and Computer Science",
-"Mechanical Engineering", 
-"eindhoven school of education"
- ) |> tolower()
+# Departments  + other units at Tu/e
+departments <- list(
+  eindhoven =  c( "Applied Physics",
+                  "Biomedical Engineering", 
+                  "Built Environment", 
+                  "Chemical Engineering",
+                  "Electrical Engineering", 
+                  "Industrial Design", 
+                  "Industrial Engineering", 
+                  "Mathematics and Computer Science",
+                  "Mechanical Engineering", 
+                  "Eindhoven School of Education"
+                ), 
+                delft = c("Aerospace Engineering"
+                          ,"Industrial Design Engineering"
+                          ,"Technology Policy and Management"
+                          ,"Electrical Engineering Mathematics and Computer Science"
+                          ,"Civil Engineering and Geoscience"
+                          ,"Mechanical Engineering"
+                          ,"Architecture and the Built Environment"
+                          ,"Applied Sciences"
+                          ,"Mechanical Maritime and Materials Engineering"
+                          ,"QuTech"
+                          ,"Kavli"
+)) 
 
- # Extract matching values from organizational lists based on TU/e departments list
+departments <- lapply(departments, tolower)
+
+
+# Extract matching values from organizational lists based on TU/e departments list
 
 datasets$org_clean <- datasets$organization_clean |>
   map(~ {
-    matches <- str_extract_all(.x, paste(departments, collapse = "|"))
+    matches <- str_extract_all(.x, paste(departments[['delft']], collapse = "|"))
     if (length(matches[[1]]) == 0) {
       .x  # Keep the original value if no match
     } else {
@@ -86,21 +134,39 @@ datasets$org_clean <- datasets$organization_clean |>
 datasets_clean <-datasets |>
   unnest(org_clean) |>
   mutate(org_clean= str_trim(org_clean)) |>
-  mutate(org_clean= org_clean |>
-                      str_replace_all("department of mathemathics and computer science", "mathematics and computer science"))|>
+  # mutate(org_clean= org_clean |> 
+  #  str_replace_all("department of mathemathics and computer science", "mathematics and computer science"))|>
+  mutate(org_clean= org_clean |> 
+   str_replace_all("mechanical maritime and materials engineering", "mechanical engineering"))|>
   distinct()
 
 datasets_clean|>
+  count(org_clean) |>
+  arrange(n) |>
+  print(n = 60)
+
+# show the resultd on a plot
+datasets_clean|>
   count(org_clean)|>
+  arrange(desc(n)) |>
+  head(10) |>
   mutate(org_clean = forcats::fct_reorder(org_clean, n)) |>
   ggplot(aes(x= org_clean, y= n)) +
   geom_col(fill = "orange") +
   coord_flip()+
-  labs(title = "TU/e dataset and software publications per department",
+  labs(title = "Dataset and software publications per department",
        subtitle = "since 01-01-2024", 
        x = "Department", 
        y = "Number of publications") +
   geom_text(aes(label = n), hjust = -0.5)+
   theme_minimal()
-  
 
+# extract civil engineering datasets 
+datasets_ceg <- datasets_clean |>
+  filter(org_clean == "civil engineering and geoscience") |>
+  select(uuid, doi, title, 
+         created_date, modified_date, organization)
+
+
+write_csv(datasets_ceg, 
+  'data/extracts/data-ceg.csv')
